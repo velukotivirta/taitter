2beb711e53ada8b0bc306cde5bf9fb9a5bf7a5b5
Vertti Koivula <vertti.koivula@gmail.com>
2025-12-10 13:18:50 +0200

    Added media changes: modified settings to upload banner images to media/

--- posts/models.py
+++ posts/models.py
@@ -8,8 +8,10 @@ class Post(models.Model):
     body = models.TextField()
     slug = models.SlugField()
     date = models.DateTimeField(auto_now_add=True)
-    banner = models.ImageField(default='fallback.png', blank=True)
+    banner = models.ImageField(default='fallback.png', blank=True, upload_to='')
     author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
 
     def __str__(self):
         return self.title # No changes to DB with function, no need for migration
+    
+    
