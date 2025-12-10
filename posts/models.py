9531c6b812bb3fcc4c860ba4324a4369f39390de
TiiaSelin <tiiaselin92@gmail.com>
2025-12-10 21:19:25 +0200

    rajattu postauksen pituus 144 merkkiin, poistettu default kuva postauksista

--- posts/models.py
+++ posts/models.py
@@ -1,17 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
+
 
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=75)
-    body = models.TextField()
-    slug = models.SlugField()
+    body = models.TextField(max_length=144)
+    slug = models.SlugField(blank=True, null=True)
     date = models.DateTimeField(auto_now_add=True)
-    banner = models.ImageField(default='fallback.png', blank=True, upload_to='')
+    banner = models.ImageField(blank=True, upload_to='')
     author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
 
     def __str__(self):
         return self.title # No changes to DB with function, no need for migration
     
-    
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.title)
+            slug = base_slug
+            counter = 1
+
+            while Post.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+
+            self.slug = slug
+
+        super().save(*args, **kwargs)
